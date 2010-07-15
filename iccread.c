bc4eafb77a3479532ab0ef968c0287a8aea9b3c2
Benoit Girard <mozilla@wavicle-2.local>
2010-07-15 00:45:15 -0400

    Fixed mAB_release and INFINITY.

--- iccread.c
+++ iccread.c
@@ -1134,9 +1134,15 @@ static void lut_release(struct lutType *lut)
 
 static void mAB_release(struct lutmABType *lut)
 {
-	free(lut->a_curves);
-	free(lut->b_curves);
-	free(lut->m_curves);
+	uint8_t i;
+
+	for (i = 0; i < lut->num_in_channels; i++){
+		free(lut->a_curves[i]);
+	}
+	for (i = 0; i < lut->num_out_channels; i++){
+		free(lut->b_curves[i]);
+		free(lut->m_curves[i]);
+	}
 	free(lut);
 }
 
--- transform_util.c
+++ transform_util.c
@@ -6,6 +6,7 @@
 #include "matrix.h"
 
 #define PARAMETRIC_CURVE_TYPE 0x70617261 //'para'
+#define QCMS_NEGATIVE_INFINITY (-1.f/0.f)
 
 /* value must be a value between 0 and 1 */
 //XXX: is the above a good restriction to have?
@@ -121,7 +122,7 @@ void compute_curve_gamma_table_type_parametric(float gamma_table[256], float par
                 c = 0;
                 e = 0;
                 f = 0;
-                interval = -INFINITY;
+                interval = QCMS_NEGATIVE_INFINITY;
         } else if(count == 1) {
                 a = parameter[1];
                 b = parameter[2];
